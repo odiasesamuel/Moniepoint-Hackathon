460c8831b9043308e922f7cc3ed39aa868d0158a
odiasesamuel <odiasesamuel4@gmail.com>
2025-02-22 14:50:33 +0100

    Implement transaction file upload functionality

--- src/app.js
+++ src/app.js
@@ -1,14 +1,39 @@
 const express = require("express");
+const multer = require("multer");
+const fs = require("fs");
+const path = require("path");
 
 const app = express();
 const PORT = 5000;
 
-// Sample route
+const uploadFolder = path.join(__dirname, "transactions");
+if (!fs.existsSync(uploadFolder)) {
+  fs.mkdirSync(uploadFolder);
+}
+
+const storage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    cb(null, uploadFolder);
+  },
+  filename: (req, file, cb) => {
+    cb(null, file.originalname);
+  },
+});
+
+const upload = multer({ storage: storage });
+
+app.post("/metrics", upload.array("files", 10), async (req, res) => {
+  if (!req.files || req.files.length === 0) {
+    return res.status(400).json({ message: "No files uploaded" });
+  } else {
+    return res.status(200).json({ message: "files uploaded" });
+  }
+});
+
 app.get("/", (req, res) => {
   res.send("Hello, Express with!");
 });
 
-// Start server
 app.listen(PORT, () => {
   console.log(`Server running on http://localhost:${PORT}`);
 });
