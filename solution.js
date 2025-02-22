501059d550961c9be9529de3cb91b0e745711f32
odiasesamuel <odiasesamuel4@gmail.com>
2025-02-22 12:22:11 +0100

    extract staffId, time, products, amount for each of the array of daily transactions

--- solution.js
+++ solution.js
@@ -12,7 +12,14 @@ let hourlyTransactionCount = {};
 function processTransactionFile(filePath) {
   const data = fs.readFileSync(filePath, "utf8");
   const lines = data.trim().split("\n");
-  console.log(lines);
+  // console.log(lines);
+  lines.forEach((line) => {
+    const [staffId, time, products, amount] = line.split(",");
+    const date = time.split("T")[0];
+    const hour = time.split("T")[1].slice(0, 2);
+    const month = date.slice(0, 7); // YYYY-MM
+    console.log(staffId, time, products, amount);
+  });
 }
 
 fs.readdirSync(transactionDir).forEach((file) => {
